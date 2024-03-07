d42229049832407134ae5acd65f00143150fabe9
BuStudios <1bustudios@gmail.com>
2024-03-07 20:49:37 +0100

    added comments

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ import discord
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv() #loads the secret files
 bot_token = os.environ["BOT_TOKEN"]
 guild_id = os.environ["GUILD_ID"]
 
 bot = discord.Bot()
 
+#logging in msg
 @bot.event
 async def on_ready():
-    print(f"We have logged in as {bot.user}")
+    print(f"logged in as {bot.user}")
 
+#test command
 @bot.slash_command(guild_ids=[guild_id])
 async def hello(ctx):
-    await ctx.respond("Hello!")
+    await ctx.respond(f"Hello {ctx.author.name}!")
 
+#for getting the active developer badge
+@bot.slash_command(guild_ids=[guild_id])
+async def active_dev(ctx):
+    await ctx.respond("https://discord.com/developers/active-developer")
+
+#runs the bot
 bot.run(bot_token)
