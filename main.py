1c5a30d983afd116cdc3d61bf4a1dccc51598750
BuStudios <1bustudios@gmail.com>
2024-03-07 16:12:02 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
+from discord.commands import Option, slash_command
 import os
 
 load_dotenv()
 bot_token = os.environ["BOT_TOKEN"]
+
+intents = discord.Intents.default()
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@slash_command(name="hello", description="Say hello to the bot")
+async def hello(ctx):
+    await ctx.respond(f"Hello, world!")
+
+bot.run(bot_token)
+
+#in development
