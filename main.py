e6d2fdf68a99ab28eaa9693763dfaa88f22187fc
BuStudios <1bustudios@gmail.com>
2024-03-08 16:47:26 +0100

    added things

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ intents.message_content = True  # This is crucial for accessing message.content
 # Initialize your bot with the specified intents
 bot = discord.Bot(command_prefix="!", intents=intents)
 
+response = {
+    "sus": "amogus",
+    "bustudios": "heleo",
+    "bubot": "yes? :robot:",
+    "ban me": "no",
+    "e": ":regional_indicator_e:"
+}
+
 # logging in msg
 @bot.event
 async def on_ready():
@@ -23,8 +31,10 @@ async def on_message(message):
     if message.author.bot:  # Ignore messages from bots
         return
     
-    if 'sus' == message.content.lower():  # Check if 'sus' is in the message
-        await message.channel.send('amogus')
+    user_message = message.content.lower()
+
+    if user_message in response: # Checks if item is in the message
+        await message.channel.send(response[user_message])
 
 # test command
 @bot.slash_command(guild_ids=[guild_id])
