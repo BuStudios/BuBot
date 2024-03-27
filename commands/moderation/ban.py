5f5c977040a32587b21febbd0d0f0d327e77681c
BuStudios <1bustudios@gmail.com>
2024-03-27 17:54:29 +0100

    🔨 better bans

--- commands/moderation/ban.py
+++ commands/moderation/ban.py
@@ -2,9 +2,17 @@ import discord
 from discord.commands import Option
 import common.emojis
 
-async def ban(ctx, member: Option(discord.User, "Select a member to ban"), reason: Option(str, "Reason for the ban")): # type: ignore
+async def ban(ctx, member: Option(discord.User, "Select a member to ban", required=True), reason: Option(str, "Reason for the ban", required=False)): # type: ignore
+    if member.id == ctx.bot.user.id:
+        await ctx.respond(f"Why would you want to ban me? {common.emojis.sad_turtle}")
+        return
+
     try:
         await member.ban(reason=reason)
         await ctx.respond(f"{common.emojis.yes} Banned <@{member.id}>! {reason}")
+
+    except discord.Forbidden:
+        await ctx.respond(f"I don't have permission to ban this member. Are they Admin? {common.emojis.hmmm}")
+
     except Exception as e:
-        await ctx.respond(f"an error occured: `{e}`")
+        await ctx.respond(f"{common.emojis.error} An error occured: `{e}`")
--- commands/moderation/timeout.py
+++ commands/moderation/timeout.py
@@ -3,9 +3,17 @@ from discord.commands import Option
 import datetime
 import common.emojis
 
-async def timeout(ctx, member: Option(discord.User, "Select a member to time out"), reason: Option(str, "Reason for the timeout")): # type: ignore
+async def timeout(ctx, member: Option(discord.User, "Select a member to time out", required=True), time: Option(str, "How long should the user be timed out?", required=True), reason: Option(str, "Reason for the timeout", required=False)): # type: ignore
+    if member.id == ctx.bot.user.id:
+        await ctx.respond(f"Why would you want to time me out? {common.emojis.sad_turtle}")
+        return
+
     try:
         await member.timeout_for(datetime.timedelta(minutes=1))
-        await ctx.respond(f"{common.emojis.yes} Banned <@{member.id}>! {reason}")
+        await ctx.respond(f"{common.emojis.yes} Timed out <@{member.id}> for 1 minute! {reason}")
+
+    except discord.Forbidden:
+        await ctx.respond(f"I don't have permission to timeout this member. Are they Admin? {common.emojis.hmmm}")
+
     except Exception as e:
-        await ctx.respond(f"an error occured: `{e}`")
+        await ctx.respond(f"{common.emojis.error} An error occured: `{e}`")
