f44c25af3f1fd01a688c2808c31007224eb57aed
BuStudios <1bustudios@gmail.com>
2024-03-27 17:59:44 +0100

    💖 banning reason defaults to ""

--- commands/moderation/ban.py
+++ commands/moderation/ban.py
@@ -2,7 +2,7 @@ import discord
 from discord.commands import Option
 import common.emojis
 
-async def ban(ctx, member: Option(discord.User, "Select a member to ban", required=True), reason: Option(str, "Reason for the ban", required=False)): # type: ignore
+async def ban(ctx, member: Option(discord.User, "Select a member to ban", required=True), reason: Option(str, "Reason for the ban", required=False) = ""): # type: ignore
     if member.id == ctx.bot.user.id:
         await ctx.respond(f"Why would you want to ban me? {common.emojis.sad_turtle}")
         return
@@ -15,4 +15,4 @@ async def ban(ctx, member: Option(discord.User, "Select a member to ban", requir
         await ctx.respond(f"I don't have permission to ban this member. Are they Admin? {common.emojis.hmmm}")
 
     except Exception as e:
-        await ctx.respond(f"{common.emojis.error} An error occured: `{e}`")
+        await ctx.respond(f"{common.emojis.error} An error occured while banning <@{member.id}>: `{e}`", ephemeral=True)
